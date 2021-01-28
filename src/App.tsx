1d1dab0b6b01f00202c23f3ecb4b5862db89716f
fadihanna123 <fhanna181@gmail.com>
2021-01-28 07:40:08 +0100

    Add list

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
-
+import { Search, IList } from "./typings/List";
 const App = () => {
-  interface Search {
-    query: string;
-  }
-
-  interface IList {
-    total: number;
-    totalHits: number;
-    hits: {
-      id: number;
-      pageURL: string;
-      type: string;
-      tags: string;
-      previewURL: string;
-      previewWidth: number;
-      previewHeight: number;
-      webformatURL: string;
-      webformatWidth: number;
-      webformatHeight: number;
-      largeImageURL: string;
-      imageWidth: number;
-      imageHeight: number;
-      imageSize: number;
-      views: number;
-      downloads: number;
-      favorites: number;
-      likes: number;
-      comments: number;
-      user_id: number;
-      user: string;
-      userImageURL: string;
-    }[];
-  }
   const [search, setSearch] = useState<Search>({
     query: "",
   });
---
+++ src/typings/List.ts
@@ -0,0 +1,32 @@
+export interface Search {
+  query: string;
+}
+
+export interface IList {
+  total: number;
+  totalHits: number;
+  hits: {
+    id: number;
+    pageURL: string;
+    type: string;
+    tags: string;
+    previewURL: string;
+    previewWidth: number;
+    previewHeight: number;
+    webformatURL: string;
+    webformatWidth: number;
+    webformatHeight: number;
+    largeImageURL: string;
+    imageWidth: number;
+    imageHeight: number;
+    imageSize: number;
+    views: number;
+    downloads: number;
+    favorites: number;
+    likes: number;
+    comments: number;
+    user_id: number;
+    user: string;
+    userImageURL: string;
+  }[];
+}
