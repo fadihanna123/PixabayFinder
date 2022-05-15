9c8ee3c20c0def943ba7094be0bc55fae4a80b63
FadiHanna <fadi.hanna@rektron.se>
2022-05-15 17:30:20 +0200

    Setup Github Secrets

--- src/utils/envs/envsVariables.ts
+++ src/utils/envs/envsVariables.ts
@@ -1,5 +1,3 @@
-const { REACT_APP_PIXABAY_BASE_URL, REACT_APP_PIXABAY_KEY } = process.env;
-
 export const PixabayBaseURL = process.env.REACT_APP_PIXABAY_BASE_URL;
 
 export const PixabayKey = process.env.REACT_APP_PIXABAY_KEY;
