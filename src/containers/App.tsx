0d2e8cde6ee84b9dff15a41989f0d668abb3c337
FadiHanna <fadi.hanna@rektron.se>
2022-10-01 18:27:16 +0200

    Organize files better

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import SearchForm from 'containers/SearchFormComp';
 import { getImages, getVideos } from 'functions';
-import Footer from 'inc/Footer';
 import { IList } from 'models';
 import { useEffect } from 'react';
 import { Flip, toast } from 'react-toastify';
@@ -12,6 +11,7 @@ import { getSearchForm } from 'redux/reducers/searchForm';
 import { setVideoList } from 'redux/reducers/videoList';
 import sal from 'sal.js';
 import { Container } from 'styles';
+import MainFooter from 'ui/MainFooter';
 
 import PhotosList from './PhotosList';
 
@@ -71,7 +71,9 @@ const App: React.FC = () => {
       <h1 data-sal='flip-left'>PixaBay Finder</h1>
       <SearchForm />
       <PhotosList />
-      <Footer />
+      <MainFooter dataSal='fade'>
+        <i>Created by Fadi Hanna.</i>
+      </MainFooter>
     </Container>
   );
 };
--- src/inc/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import MainFooter from 'ui/MainFooter';
-
-const Footer: React.FC = () => (
-  <MainFooter dataSal='fade'>
-    <i>Created by Fadi Hanna.</i>
-  </MainFooter>
-);
-
-export default Footer;
