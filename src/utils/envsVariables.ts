1c638e90ceb222ff2dc746eac7c6c5a4dcee64a0
unknown <Fadi Hanna>
2023-02-06 16:23:50 +0100

    Deploy test

--- src/utils/envsVariables.ts
+++ src/utils/envsVariables.ts
@@ -8,3 +8,5 @@ export const PixabayBaseURL: string | undefined =
  * @type { string | undefined }
  */
 export const PixabayKey: string | undefined = process.env.REACT_APP_PIXABAY_KEY;
+
+console.log(process.env.REACT_APP_PIXABAY_KEY);
