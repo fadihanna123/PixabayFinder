8e60e8d40484bfb295ec029de563152775d2a868
fadihanna123 <fadialias230@gmail.com>
2023-03-11 19:03:26 +0100

    Handle api errors and add tasks

--- src/functions/apiStore.ts
+++ src/functions/apiStore.ts
@@ -1,10 +1,33 @@
+/* eslint-disable no-console */
 import { request } from 'api';
+import { AxiosError } from 'axios';
 import { IList } from 'models';
 import { PixabayKey } from 'utils';
 
 /**
  * @author Fadi Hanna <fhanna181@gmail.com>
  */
+
+/**
+ * Handle errors coming from api requests.
+ *
+ * @function requestErrorHandler
+ * @param { AxiosError } err - Error object
+ * @returns { void }
+ */
+const requestErrorHandler = (err: AxiosError): void => {
+  if (err.response) {
+    console.log(err.response.data);
+    console.log(err.response.status);
+    console.log(err.response.headers);
+  } else if (err.request) {
+    console.log(err.request);
+  } else {
+    console.log('Error', err.message);
+  }
+
+  console.log(err.config);
+};
 
 /**
  * Get imagelist from api.
@@ -14,10 +37,14 @@ import { PixabayKey } from 'utils';
  * @param { string } imageVal - Image value
  * @returns { Promise<any> } Promise contains images array.
  */
-
 export const getImages = async <T>(imageVal: T): Promise<any> => {
   const endPoint: string = `?key=${PixabayKey}&q=${imageVal}`;
-  return request.get<IList[]>(endPoint);
+
+  try {
+    return await request.get<IList[]>(endPoint);
+  } catch (err) {
+    requestErrorHandler(err as AxiosError);
+  }
 };
 
 /**
@@ -25,10 +52,15 @@ export const getImages = async <T>(imageVal: T): Promise<any> => {
  *
  * @async
  * @function getVideos
- * @param {string} val - Video value
+ * @param { string } val - Video value
  * @returns { Promise<any> } Promise contains video object
  */
 export const getVideos = async <T>(val: T): Promise<any> => {
   const endPoint: string = `videos/?key=${PixabayKey}&q=${val}`;
-  return await request.get<IList>(endPoint);
+
+  try {
+    return await request.get<IList>(endPoint);
+  } catch (err) {
+    requestErrorHandler(err as AxiosError);
+  }
 };
