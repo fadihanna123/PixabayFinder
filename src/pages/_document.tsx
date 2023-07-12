ebca4107c4f242eb6923f3485ebe78323344a5f9
fadihanna123 <fadialias230@gmail.com>
2023-07-12 03:25:16 +0200

    Add keys check

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { toast } from 'react-toastify';
+
+const { NEXT_PUBLIC_PIXABAY_KEY } = process.env;
+
+if (!NEXT_PUBLIC_PIXABAY_KEY) {
+  toast.error('Missing apiKey? Add it and restart the app!');
+  throw new Error('Missing apiKey? Add it and restart the app!');
+}
 
 const Document = () => (
   <Html>
