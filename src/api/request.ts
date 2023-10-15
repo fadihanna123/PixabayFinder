2044b28f6824a5afb800f6220c38a490cdbc0d0f
fadihanna123 <54924938+fadihanna123@users.noreply.github.com>
2023-10-15 20:33:32 +0200

    Make Rest api more clean

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { PixabayBaseURL } from 'utils';
 
+axios.defaults.baseURL = PixabayBaseURL;
+
 /**
  * Request object to manipulate api.
  * @param {{ get: Promise<T | void>, post: Promise<T | void>, put: Promise<T | void>, delete: Promise<T | void> }} Request
@@ -10,7 +12,7 @@ export const request = {
     url: string,
     headers?: Record<string, never>
   ): Promise<T | void> => {
-    const { data } = await axios.get<T>(`${PixabayBaseURL}${url}`, headers);
+    const { data } = await axios.get<T>(`${url}`, headers);
     return data;
   },
 
@@ -19,11 +21,7 @@ export const request = {
     redata: unknown,
     headers?: { headers: Record<string, never> }
   ): Promise<T | void> => {
-    const { data } = await axios.post<T>(
-      `${PixabayBaseURL}${url}`,
-      redata,
-      headers
-    );
+    const { data } = await axios.post<T>(`${url}`, redata, headers);
     return data;
   },
 
@@ -32,13 +30,13 @@ export const request = {
     redata: unknown,
     headers?: { headers: Record<string, never> }
   ): Promise<T | void> => {
-    await axios.put<T>(`${PixabayBaseURL}${url}`, redata, headers);
+    await axios.put<T>(`${url}`, redata, headers);
   },
 
   delete: async <T>(
     url: string,
     headers?: { headers: Record<string, never> }
   ): Promise<T | void> => {
-    await axios.delete<T>(`${PixabayBaseURL}${url}`, headers);
+    await axios.delete<T>(`${url}`, headers);
   },
 };
